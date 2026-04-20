16760a6afa58b3328453f4d6ba219b6845cccd7c
Matt Briscoe <mbriscoe@adobe.com>
2026-04-20 11:16:36 -0600

    Updates

--- examples/angular/src/environments/environment.prod.ts
+++ examples/angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  imsClientId: "<REPLACE_WITH_ASSET_SELECTOR_IMS_CLIENT_ID>",
+  imsClientId: "<YOUR_IMS_CLIENT_ID>",
   imsOrg: null,
 };
--- examples/angular/src/environments/environment.ts
+++ examples/angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  imsClientId: "<REPLACE_WITH_ASSET_SELECTOR_IMS_CLIENT_ID>",
+  imsClientId: "<YOUR_IMS_CLIENT_ID>",
   imsOrg: null,
 };
